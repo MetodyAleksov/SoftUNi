9f92300601da0add612e225c77a1ddaeb73c84d5
MetodyAleksov <71280200+MetodyAleksov@users.noreply.github.com>
2020-09-16 19:34:22 +0300

    BasicSyntax

---
+++ BasicSyntax/03.Vacation/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Textadventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfPeople = int.Parse(Console.ReadLine());
+            string type = Console.ReadLine();
+            string dayOfWeek = Console.ReadLine();
+            double totalPrice = 0;
+
+            if (type == "Students")
+            {
+                if (dayOfWeek == "Friday")
+                {
+                    totalPrice = numOfPeople * 8.45;
+                }
+                else if (dayOfWeek == "Saturday")
+                {
+                    totalPrice = numOfPeople * 9.80;
+                }
+                else if (dayOfWeek == "Sunday")
+                {
+                    totalPrice = numOfPeople * 10.46;
+                }
+                if (numOfPeople >= 30)
+                {
+                    totalPrice = totalPrice * 0.85;
+                }
+            }
+            else if (type == "Business")
+            {
+                if (numOfPeople >= 100)
+                {
+                    numOfPeople -= 10;
+                }
+                if (dayOfWeek == "Friday")
+                {
+                    totalPrice = numOfPeople * 10.90;
+                }
+                else if (dayOfWeek == "Saturday")
+                {
+                    totalPrice = numOfPeople * 15.60;
+                }
+                else if (dayOfWeek == "Sunday")
+                {
+                    totalPrice = numOfPeople * 16;
+                }
+            }
+            else if (type == "Regular")
+            {
+                if (dayOfWeek == "Friday")
+                {
+                    totalPrice = numOfPeople * 15;
+                }
+                else if (dayOfWeek == "Saturday")
+                {
+                    totalPrice = numOfPeople * 20;
+                }
+                else if (dayOfWeek == "Sunday")
+                {
+                    totalPrice = numOfPeople * 22.50;
+                }
+                if (numOfPeople >= 10 && numOfPeople <= 20)
+                {
+                    totalPrice *= 0.95;
+                }
+            }
+            Console.WriteLine($"Total price: {totalPrice:f2}");
+        }
+    }
+}
---
+++ BasicSyntax/05.Login/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _05.Login
+{ 
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = Console.ReadLine();
+            string password = "";
+            for (int i = username.Length-1; i <= username.Length; i--)
+            {
+                password += username[i];
+            }
+            Console.WriteLine(password);
+        }
+    }
+}
